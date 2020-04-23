b25a5772c8ce9b1626032704a56f52cb04732020
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-04-23 15:04:05 +0200

    readme pic

--- Client/app/src/App.js
+++ Client/app/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2 className="title">Github Acount Motor Search</h2>
+      <h2 className="title">Github Account Motor Search</h2>
       <Stars />
       <Rep />
     </div>
